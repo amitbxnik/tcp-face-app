0e4f5bda99bab172fc1f34da46dd956f6783cd28
neha-ct <ct.neha.22@gmail.com>
2025-04-30 19:43:26 -0400

    Moved DockerFile + Procfile into 'deploy' folder, moved app.py into 'old_approach' folder

--- TCP_facial_recognition_project/settings.py
+++ TCP_facial_recognition_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["web-production-4f8ce.up.railway.app"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 CSRF_TRUSTED_ORIGINS = ["https://web-production-4f8ce.up.railway.app"]
 
 
